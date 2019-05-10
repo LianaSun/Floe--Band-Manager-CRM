6932db2fb32e0d9d94de4ad8b32373b8f9019bb5
David Vo <davidvo1990@gmail.com>
2019-05-09 19:44:32 -0500

    work on post

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,42 @@
-var db = require("../models");
+var path = require("path");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+module.exports = function (app) {
+  // Loads index page
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/floe.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/cms", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/cms.html"));
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // floe route loads floe.html
+  app.get("/floe", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/floe.html"));
   });
+
+  // artists route loads artist-manager.html
+  app.get("/artists", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/artist-manager.html"));
+  });
+
+  // Loads Artist page and pass in an example by id
+  // app.get("/artists", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/html/artist.html"));
+  // });
+
+  // Loads Gigs ("shows") page and pass in an example by id
+  // app.get("/gigs", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/html/show.html"));
+  // });
+
+  // // Loads Venues page and pass in an example by id
+  // app.get("/venues", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/html/venue.html"));
+  // });
+
+  // Render homepage for any unmatched routes
+  // app.get("*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/html/floe.html"));
+  // });
 };
